4bc3182d9635b5d8c6db08039002c24cbbd5045b
ksamuk <ksamuk@gmail.com>
2016-01-26 17:23:28 -0800

    plot structure

--- 04_plot_structure_output.R
+++ 04_plot_structure_output.R
@@ -4,12 +4,13 @@ library(tidyr)
 library(ggplot2)
 library(stringr)
 library(wesanderson)
+library(grid)
 
 list.files("functions", full.names = TRUE) %>% sapply(.,source, verbose = FALSE, echo = FALSE) %>% invisible
 
 # plot structure output
 #structure_files <- list.files("data/structure/results_rename", pattern = "simple.[0-9].meanQ",full.names = TRUE)
-structure_files <- list.files("data/structure/results_full", pattern = "simple.[0-9].meanQ",full.names = TRUE)
+structure_files <- list.files("data/structure/results", pattern = "simple_noXIX.[0-9].meanQ",full.names = TRUE)
 
 
 # read in base metadata file
@@ -22,35 +23,44 @@ str_k4 <- read_structure(structure_files[4], meta_df)
 str_k5 <- read_structure(structure_files[5], meta_df)
 str_k6 <- read_structure(structure_files[6], meta_df)
 
+k1_vals <- str_k2 %>%
+  filter(year != 2012) %>%
+  filter(k == "k1") %>%
+  select(id, q.value) %>%
+  rename(k1_val = q.value )
+
 k3_vals <- str_k4 %>%
   filter(year != 2012) %>%
   filter(k == "k3") %>%
   select(id, q.value) %>%
   rename(k3_val = q.value )
 
-tmp <- left_join(str_k4, k3_vals)
+tmp <- left_join(str_k2, k1_vals)
+tmp <- left_join(tmp, k3_vals)
 
 
 tmp %>%
 	filter(year != 2012) %>%
-  group_by(sex) %>%
+  group_by(id) %>%
   mutate(max_q = max(q.value)) %>% 
   mutate(major_k = k[which(q.value == max_q)]) %>%
-  #mutate(id = reorder(id, as.numeric(major_k))) %>%
-  mutate(id = reorder(id, as.numeric(k3_val))) %>%
-  #filter(!pop=="MM",!pop=="NHR",!pop=="QR",!pop=="MM", !pop=="LD", !pop=="CB", !pop=="SP") %>%
+  ungroup()%>%
+  mutate(pop = ifelse(grepl("CP", pop), "CP", as.character(pop))) %>%
+  mutate(id = reorder(id, as.numeric(k1_val))) %>%
+  filter(!pop=="MM",!pop=="NHR",!pop=="QR",!pop=="MM", !pop=="LD", !pop=="CB", !pop=="SP") %>%
   ggplot(aes(x = id, y = q.value, fill = factor(k)))+
   #ggplot(aes(x=id, y=q.value, fill=factor(pop)))+
   geom_bar(stat="identity", width=1, color = "black")+
   #geom_bar(aes(x=id,fill=pop, y=.01),stat="identity",width=1,position="stack")+
   #geom_text(aes(label=pop))+
   theme_classic()+
-  theme(axis.text=element_blank(), 
-        axis.ticks=element_blank(), 
-        axis.line=element_blank(),
-        axis.title=element_blank(),
-        strip.text=element_text(size=12))+
-  facet_wrap(~gen.sex, scales = "free_x")+
+  theme(axis.text = element_blank(), 
+        axis.ticks = element_blank(), 
+        axis.line = element_blank(),
+        axis.title = element_blank(),
+        strip.text = element_text(size=12),
+        panel.margin = unit(0.1, "lines"))+
+  facet_wrap(pop~gen.sex, scales = "free_x")+
 	scale_fill_brewer(palette = "Set1")
 
 ## Prepare meta data (as harvested from structure)
