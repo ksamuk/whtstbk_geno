45620b451937f29ce5529f2df0fccca0ac08a8b8
ksamuk <ksamuk@gmail.com>
2016-03-16 10:51:28 -0700

    site pi metrics

--- 10_fdr_outlier_windows.R
+++ 10_fdr_outlier_windows.R
@@ -85,16 +85,15 @@ out_long %>%
 
 # force name compatibility
 names(out_df)[2] <- "pos1"
-stat_df$chr <- stat_df$chr %>% gsub("chr", "", .) %>% gsub("Un", "XXII", .) %>% as.roman %>% as.numeric
+wind_df$chr <- wind_df$chr %>% gsub("chr", "", .) %>% gsub("Un", "XXII", .) %>% as.roman %>% as.numeric
 
-sel_df <- left_join(out_df %>% select(chr, pos1, matches("adj")), stat_df %>% select(-pos2))
+sel_df <- left_join(out_df %>% select(chr, pos1, matches("adj")), wind_df %>% select(-pos2))
 sel_long <- gather(sel_df, key = stat, value = value, -chr, -pos1, -matches("adj"))
 
 sel_long %>% 
   ggplot(aes(x = fst_outlier_adj, y = value, fill = stat))+
   geom_violin()+
   facet_wrap(~stat, scales = "free_y")
-
 
 # compare ihs/ies/rsb/load/r2 SNP stats for windows
 
@@ -103,9 +102,27 @@ snp_df <- read.table("data/stats/snp_stats_master.txt", header = TRUE, stringsAs
 snp_df$pos2 <- (((snp_df$pos / 75000) %>% floor) + 1)*75000
 snp_df$pos1 <- snp_df$pos2 - 74999
 
-
 #snp_df <- left_join(out_df %>% select(-matches("qval")), snp_df %>% select(-pos2, -matches("xtx|fst|outlier|pval")))
 snp_df <- left_join(out_df %>% select(-matches("qval")), snp_df %>% select(-pos2))
+
+
+# pi site data
+wht_pi <- read.table("data/stats/wht_pi.site.2014.txt", header = TRUE, stringsAsFactors = FALSE)
+cmn_pi <- read.table("data/stats/cmn_pi.site.2014.txt", header = TRUE, stringsAsFactors = FALSE)
+cbr_pi <- read.table("data/stats/cbr_pi.site.2014.txt", header = TRUE, stringsAsFactors = FALSE)
+
+pi_df <- left_join(wht_pi, cmn_pi, by = c("CHROM", "POS"))
+pi_df <- left_join(pi_df, cbr_pi, by = c("CHROM", "POS"))
+
+names(pi_df) <- c("chr", "pos", "pi_site_wht", "pi_site_cmn", "pi_site_cbr")
+
+pi_df$chr <- pi_df$chr %>% gsub("chr", "", .) %>% gsub("Un", "XXII", .) %>% as.roman %>% as.numeric
+
+snp_df <- left_join(snp_df, pi_df)
+
+################################################################################
+# Synthetic variables
+################################################################################# 
 
 snp_df$load_abs_ev1 <- abs(snp_df$load_ev1)
 snp_df$load_abs_ev2 <- abs(snp_df$load_ev2)
@@ -119,6 +136,7 @@ snp_df$ihs_abs_wht <- abs(snp_df$ihs_wht)
 snp_df$ihs_abs_cmn <- abs(snp_df$ihs_cmn)
 snp_df$ihs_abs_delta <- snp_df$ihs_abs_wht - snp_df$ihs_abs_cmn
 
+
 # join in windowed stats
 wind_df$chr <- wind_df$chr <- wind_df$chr %>% gsub("chr", "", .) %>% gsub("Un", "XXII", .) %>% as.roman %>% as.numeric
 
@@ -126,11 +144,15 @@ snp_df <- left_join(snp_df, wind_df %>% select(-matches("fst|r2")))
 
 snp_long <- gather(snp_df , key = stat, value = value, -chr, -pos1, -pos, -matches("adj"))
 
+################################################################################
+# plots
+################################################################################# 
+
 # whole chromosome view
 snp_long %>% 
-  filter(chr == 18) %>%
+  filter(chr == 4) %>%
   filter(!grepl("cbr", stat)) %>%
-  filter(grepl("rsb_wht_cmn|fst_wht_cmn|fst_cbr_cmn|tajd_wht|tajd_cmn|pi_wht|pi_cmn", stat)) %>%
+  filter(grepl("rsb_wht_cmn|fst_wht_cmn|fst_cbr_cmn|tajd_wht|tajd_cmn|pi_site_wht|pi_site_cmn", stat)) %>%
   ggplot(aes(x = pos, y = value, color = xtx_outlier_adj))+
   geom_point(alpha = 1)+
   facet_grid(stat~., scales = "free_y")+
@@ -147,7 +169,7 @@ snp_long %>%
 snp_long %>% 
   #filter(chr == 21) %>%
   filter(!grepl("cbr", stat)) %>%
-  filter(grepl("rsb_wht_cmn|fst_wht_cmn|fst_cbr_cmn|tajd_wht|tajd_cmn|pi_wht|pi_cmn", stat)) %>%
+  filter(grepl("rsb_wht_cmn|fst_wht_cmn|fst_cbr_cmn|tajd_wht|tajd_cmn|pi_site_wht|pi_site_cmn", stat)) %>%
   mutate(outlier = fst_outlier_adj|xtx_outlier_adj) %>%
   ggplot(aes(x = outlier, y = value, color = stat))+
   geom_boxplot()+
